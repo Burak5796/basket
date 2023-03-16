404c4cea217dbc5d8ead144b8da9148a4820d916
Burak5796 <burak.d5796@gmail.com>
2023-03-16 11:42:06 +0100

    bootstrap

--- myapp/src/pages/main-components/Navbar.jsx
+++ myapp/src/pages/main-components/Navbar.jsx
@@ -3,11 +3,12 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import '../../styles/start.css';
 
 function NavScrollExample() {
   return (
-    <Navbar bg="light" expand="lg">
+    <div className='nav-container'>
+    <Navbar bg="primary" expand="lg">
       <Container fluid>
         <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
@@ -19,17 +20,8 @@ function NavScrollExample() {
           >
             <Nav.Link href="#action1">Home</Nav.Link>
             <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
+            
+            <Nav.Link href="#" >
               Link
             </Nav.Link>
           </Nav>
@@ -40,11 +32,12 @@ function NavScrollExample() {
               className="me-2"
               aria-label="Search"
             />
-            <Button variant="outline-success">Search</Button>
+            <Button variant="outline-light">Search</Button>
           </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    </div>
   );
 }
 
